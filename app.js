94245b214dd95523243759024001e7b45af8e7be
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 00:52:13 +0300

    Типы данных

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const isSuited = 100 - 10 > 90 - 5;
-console.log(isSuited);
+let a = 5;
+let b = 5.6;
+console.log(typeof a);
+console.log(typeof b);
+a = 'строка';
+console.log(typeof a);
+e = `sadasd`;
+console.log(typeof e);
 
-const a = (6 + 10) / 2;
-console.log(a);
+let isAdmin = a > 10;
+console.log(typeof isAdmin);
 
-let b;
 let c;
-c = b = 100;
-console.log(c);
-console.log(b);
+console.log(typeof c);
+c = 5;
+console.log(typeof c);
 
-
+let d = null;
+console.log(d == null);
