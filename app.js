e816596041d74e9a75f5774cba192043e5982ac5
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 12:42:42 +0300

    Упражнение - Функция разобра URL

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const data = [1, 2, 3, 4, 5, 6];
-const [one, two, ...others] = data;
-console.log(one, two, others);
+const url = 'https://purpleschool.ru/course/javascript';
+
+const urlArray = url.split('/');
+const emptyEl = urlArray.indexOf('');
+const dotted = urlArray.indexOf(':');
+if (emptyEl) {
+  urlArray.splice(emptyEl, 1);
+}
+const [protocol, domenName, ...pathToFile] = urlArray;
+const currentPath = pathToFile.join('/');
+const protocolName = protocol.slice(0, -1)
+
+
+function getUrlParts(url) {
+  const [protocol, _, host, ...path] = url.split('/');
+  if (protocol === 'https:' || protocol === 'http:') {
+    if (!host.includes('.')) {
+      return;
+    }
+    console.log(protocol, host, path);
+    console.log(`Протокол: ${protocol.split(':')[0]}`);
+    console.log(`Доменное имя: ${host}`);
+    console.log(`Путь: /${path.join('/')}`);
+  } else {
+    console.log('Некоректные данные')
+  }
+
+}
+
+getUrlParts(url);
