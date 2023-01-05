5dc8a35333cff86f75a9a581826ed8a9438f630b
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 13:38:07 +0300

    Упражнение - Расчет итогового баланса

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const arr = [1, 4, 8, 7];
+const operations = [1000, -700, 300, -500, 10000];
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-};
-
-
-for (let element of arr) {
-  console.log(element);
+const resultBalance = arr => {
+  let balance = 100;
+  for (let element of operations) {
+    balance += element;
+  };
+  return balance;
+}
+const countMoney = arr => {
+  let result = 100;
+  let isOk = true;
+  for (let element of arr) {
+    result += element;
+    if (result < 0) {
+      isOk = false;
+      break
+    }
+  }
+  return isOk;
 }
 
-for (let index in arr) {
-  console.log(arr[index]);
+const getAdvancedSumOfAdded = arr => {
+  let added = [];
+  let odd = [];
+  for (let element of operations) {
+    element < 0 ? odd.push(element) : added.push(element);
+  }
+  let resultAdded = 0;
+  for (let element of added) {
+    resultAdded += element;
+  }
+
+  let resultOdd = 0;
+  for (let element of odd) {
+    resultOdd += element;
+  }
+  resultOdd = resultOdd / odd.length;
+  resultAdded = resultAdded / added.length;
+  console.log(`Средний доход : ${resultAdded}`);
+  console.log(`Средний расход : ${resultOdd}`);
 }
+
+console.log(resultBalance(operations));
+console.log(countMoney(operations));
+getAdvancedSumOfAdded(operations);
+
+
+
