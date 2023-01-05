a98e914055f9575204ef1cc038c3f9e795ba72d5
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 15:27:45 +0300

    Быстрое создание массивов

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-const users = ['Вася', 'Маша', 'Катя', 'Аня'];
+const arr = [1, 2, 3, 4, 5];
 
-console.log(users);
-users.sort();
-console.log(users);
+console.log(new Array(1, 2, 3, 4, 5));
+const arr2 = new Array(5);
+arr2.fill(1, 0, 3);
+// console.log(arr2.map(el => 1))
 
-
-const operations = [100, -300, -100, 50, 480];
-console.log(operations);
-operations.sort((a, b) => {
-  if (a > b) {
-    return -1
-  } else if (a < b) {
-    return 1
-  } else {
-    return 0
-  }
-});
-console.log(operations);
+const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1);
+console.log(arr3);
