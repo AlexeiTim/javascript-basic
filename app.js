3ad40007391fb07debdf6ff22b09a78ea9ae82e5
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 09:13:19 +0300

    Операторы равенства

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const count = 12000;
-const procentOnYear = 7;
-const waitingMoney = 13500;
-const waitingTime = 2 * 12;
-const currentMoney = count * (1 + procentOnYear / 100 / 12) ** waitingTime;
-console.log(currentMoney)
-if (currentMoney > waitingMoney) {
-  console.log(`Сможет купить дом за ${waitingMoney}$, остаток ${currentMoney - waitingMoney}. Мы накопили ${currentMoney}`)
-} else {
-  console.log(`Не сможет купить дом за ${waitingMoney}`)
+const secretNumber = '7';
+
+if (Number(secretNumber) === 7) {
+  console.log('Угадал строго')
+}
+
+if (secretNumber == 7) {
+  console.log('Угадал не строго ')
 }
