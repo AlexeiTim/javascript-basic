21683427450c9be0071fb43fc10642db9550eac2
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 15:19:22 +0300

    Метод sort

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const prices = [[2, [3, [2, 3]]], [3, 4], [10, 20]];
-const result = prices.flat(Infinity);
-const res2 = prices.flatMap(el => {
-  return el.concat([1]);
-})
-// const resultFlatMap = prices.flatMap(1);
-console.log(result)
-// console.log(resultFlatMap)
+const users = ['Вася', 'Маша', 'Катя', 'Аня'];
+
+console.log(users);
+users.sort();
+console.log(users);
+
+
+const operations = [100, -300, -100, 50, 480];
+console.log(operations);
+operations.sort((a, b) => {
+  if (a > b) {
+    return -1
+  } else if (a < b) {
+    return 1
+  } else {
+    return 0
+  }
+});
+console.log(operations);
