1b216b47a255f544a44150ebecc51d80cb9b5272
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 09:26:07 +0300

    Switch

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const secretNumber = '7';
+const role = 'ceo';
 
-// if (Number(secretNumber) === 7) {
-//   console.log('Угадал строго')
+
+// switch (role) {
+//   case 'manager':
+//     console.log('Менеджер'); // role === 'manager'
+//     break
+//   case 'admin':
+//     console.log('Админ');
+//     break
+//   case 'ceo':
+//     console.log('CEO');
+//     break;
+//   default:
+//     console.log('Мы тебя не знаем');
+//     break
 // }
 
-// if (secretNumber == 7) {
-//   console.log('Угадал не строго ')
-// }
+switch (role) {
+  case 'manager':
+  case 'admin':
+    console.log('Не руководитель');
+    break
+  case 'ceo':
+    console.log('Руководитель');
+    break;
+  default:
+    console.log('Мы тебя не знаем');
+    break
+}
 
-const q = Number(prompt('Введите число'));
 
-if (q === 7) {
-  console.log('Угадал строго')
+const num = 0;
+
+switch (true) {
+  case num > 0:
+    console.log('Положительный')
+    break;
+  case num < 0:
+    console.log('Отрицательный');
+    break;
+  default:
+    console.log('Ноль!');
 }
