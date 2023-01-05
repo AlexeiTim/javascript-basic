2ce44c72dc83f29e6903dea81765f367ee5b7ed8
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 17:15:25 +0300

    Упражнение - преобразование объектов

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const users = [
-  { name: 'Вася', age: 30 },
-  { name: 'Катя', age: 18 },
-  { name: 'Аня', age: 40 },
-  { name: 'Петя', age: 25 },
-]
+  {
+    name: 'Вася',
+    surname: 'Пупкин',
+    age: 30,
+    skills: ['Разработка', 'DevOps'],
+  },
+  {
+    name: 'Катя',
+    surname: 'Белова',
+    age: 30,
+    skills: ['Дизайн'],
+  }
+];
 
-console.log(users.sort((a, b) => a.age - b.age));
+const res = users
+  .map(user => {
+    return {
+      fullName: `${user.name} ${user.surname}`,
+      skills: user.skills.length,
+    }
+  });
+console.log(res);
