90ebe058242d4441e30f9779430f30162bdf9c7e
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 14:51:07 +0300

    Метод Reduce

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-const operations = [100, -20, 7, -20, 50];
+const operations = [100, -20, 7, -30, 50];
 
-// const positiveOperation = [];
-// for (const operation of operations) {
-//   if (operation > 0) {
-//     positiveOperation.push(operation);
-//   }
-// }
+let balance = 0;
+for (const operation of operations) {
+  balance += operation;
+}
 
-// console.log(positiveOperation);
+let result = operations.reduce((acc, el) => acc += el, 0)
+console.log(result);
 
-const prices = [[100, 200], [120, 100], [200, 350]];
+const minElement = operations.reduce((acc, operation, i) => {
+  return operation < acc ? acc : operation
+}, 0);
 
-// const positiveChangingOfPrice = prices
-//   .filter(el => {
-//     return el[0] < el[1]
-//   })
-//   .map(el => {
-//     return el[1] - el[0]
-//   })
-
-const positiveChangingOfPrice = prices
-  .map(el => el[1] - el[0])
-  .filter(el => el > 0);
-
-console.log(positiveChangingOfPrice);
+console.log(minElement)
