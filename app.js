8827f4597a1b8c93031adfd65ced703ef06a2a4d
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 14:11:39 +0300

    Из обычной функции высшего порядка с замыканием в стрелочную функцию

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-function power(pow) {
-  return function (num) {
-    return num ** pow;
-  }
-}
-
-
+// function power(pow) {
+//   return function (num) {
+//     return num ** pow;
+//   }
+// }
+const power = pow => (num) => num**pow;
 const powerOfTwo = power(2);
-console.log(powerOfTwo(5));
-console.log(powerOfTwo(10));
+console.log(powerOfTwo(5))
+// const powerOfTwo = power(2);
+// console.log(powerOfTwo(5));
+// console.log(powerOfTwo(10));
 
-const powerOfThree = power(3);
-console.log(powerOfThree(5));
-console.log(powerOfThree(10));
+// const powerOfThree = power(3);
+// console.log(powerOfThree(5));
+// console.log(powerOfThree(10));
 
-console.log(power(5)(4))
+// console.log(power(5)(4));
 
