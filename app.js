1ea69587126f54444956127d44e43e9b0b60a406
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 15:01:16 +0300

    Reduce - avg value

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const operations = [100, -20, 7, -30, 50];
-
-let balance = 0;
-for (const operation of operations) {
-  balance += operation;
-}
-
-let result = operations.reduce((acc, el) => acc += el, 0)
-console.log(result);
-
-const minElement = operations.reduce((acc, operation, i) => {
-  return operation < acc ? acc : operation
+const arr = [2, 4, 4, 10];
+const average = arr.reduce((acc, el, i) => {
+  if (i != arr.length - 1) {
+    return acc += el;
+  } else {
+    return (acc + el) / 2
+  }
 }, 0);
 
-console.log(minElement)
+console.log(average);
