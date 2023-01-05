4d6c70c51a2090712906993f4a28884e2a728f29
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 13:06:46 +0300

    Цикл в цикле

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
-
-const arr = ['!', 'JS', 'люблю', 'Я'];
-const result = [];
-for (let i = arr.length - 1; i >= 0; i--) {
-  result.push(arr[i]);
+for (let i = 1; i < 5; i++) {
+  console.log(`Цикл 1 - ${i}`);
+  for (let j = 1; j < 5; j++) {
+    console.log(`Цикл 2 - ${j}`)
+  }
 }
 
-console.log(result.join(' '));
+const tasks = [[1, 'Задача 1'], [2, 'Задача 2']];
+
+for (let i = 0; i < tasks.length; i++) {
+  console.log(tasks[i]);
+  for (let j = 0; j < tasks[i].length; j++) {
+    console.log(tasks[i][j]);
+  }
+}
