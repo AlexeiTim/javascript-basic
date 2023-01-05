b177dd665df6532a7415f4e83bb0a3cc906bdafd
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 10:00:26 +0300

    Операторы с другими типами

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+console.log('Вася' || 'Олег');
+console.log(false || 'Олег');
+console.log('Вася' || false);
+console.log(false || false);
+
+console.log('Вася' && 'Олег');
+console.log(false && 'Олег');
+console.log('Вася' && false);
+console.log(false && false);
+
+
+let a;
+const userName = a || 'Петя';
+console.log(userName);
+
 const isAdmin = true;
-const canWrite = true;
+const filename = isAdmin && 'file.mp4';
+console.log(filename);
 
-console.log(`Системный файл ${isAdmin && canWrite}`);
-console.log(`Обычный файл ${isAdmin || canWrite}`);
-console.log(`Инвентируем права админа ${!isAdmin}`);
 
-const isEdited = true;
-const isSuperAdmin = true;
 
-console.log(`Файл с редактированием ${isAdmin && canWrite && (!isEdited || isSuperAdmin)
-  }`)
-
-let a = 7;
-if (a === -8 || a === 22 ) {
-  console.log('Я не робот');
-}
