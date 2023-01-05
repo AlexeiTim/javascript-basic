4704860efbbf7239221fc88deac85641a9013add
AlexeiTim <timashkov060896@gmail.com>
2023-01-06 00:48:37 +0300

    Замыкание упражнение

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-// 'use strict'
-
-function changeBalance() {
-  let balance = 0;
-  return function (sum) {
-    balance += sum;
-    console.log(balance);
+const userInfo = {
+  balance: 0,
+  operations: 0,
+  increase(sum) {
+    this.balance += sum;
+    this.operations++;
+    console.log(this.balance, this.operations);
   }
 }
 
-const change = changeBalance();
-change(100);
-change(-50);
-change(200);
-change(200);
+function user() {
+  const userObj = {
+    balance: 0,
+    operations: 0,
+    increase(sum) {
+      this.balance += sum;
+      this.operations++;
+      console.log(this.balance, this.operations);
+    }
+  }
+  return function () {
+    return userObj;
+  }
+}
 
-console.dir(change);
+const user1 = user(userInfo);
+user1().increase(100);
+user1().increase(100);
 
-const change2 = changeBalance();
-change2(100);
-console.dir(change2);
+const user2 = user(userInfo);
+user2().increase(200);
+user2().increase(200);
+user2().increase(200);
+
+console.log(user1());
+console.log(user2());
