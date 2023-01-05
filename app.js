a42bea126f2f5cd44bcb3122bf1aec396ca765ff
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 09:15:53 +0300

    Операторы равенста + prompt

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const secretNumber = '7';
 
-if (Number(secretNumber) === 7) {
-  console.log('Угадал строго')
-}
+// if (Number(secretNumber) === 7) {
+//   console.log('Угадал строго')
+// }
 
-if (secretNumber == 7) {
-  console.log('Угадал не строго ')
+// if (secretNumber == 7) {
+//   console.log('Угадал не строго ')
+// }
+
+const q = Number(prompt('Введите число'));
+
+if (q === 7) {
+  console.log('Угадал строго')
 }
