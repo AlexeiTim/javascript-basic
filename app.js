4f91e36257e6793fbc2aa50aac3aa06251b620df
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 12:51:16 +0300

    Цикл for

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-const url = 'https://purpleschool.ru/course/javascript';
-
-const urlArray = url.split('/');
-const emptyEl = urlArray.indexOf('');
-const dotted = urlArray.indexOf(':');
-if (emptyEl) {
-  urlArray.splice(emptyEl, 1);
+for (let i = 1; i < 10; i++) {
+  console.log(`Наш баланс ${i}`);
 }
-const [protocol, domenName, ...pathToFile] = urlArray;
-const currentPath = pathToFile.join('/');
-const protocolName = protocol.slice(0, -1)
-
-
-function getUrlParts(url) {
-  const [protocol, _, host, ...path] = url.split('/');
-  if (protocol === 'https:' || protocol === 'http:') {
-    if (!host.includes('.')) {
-      return;
-    }
-    console.log(protocol, host, path);
-    console.log(`Протокол: ${protocol.split(':')[0]}`);
-    console.log(`Доменное имя: ${host}`);
-    console.log(`Путь: /${path.join('/')}`);
-  } else {
-    console.log('Некоректные данные')
-  }
-
-}
-
-getUrlParts(url);
