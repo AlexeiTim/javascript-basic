95398af839f0cc3b9c7c0519648aaec01d5062b9
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 18:45:26 +0300

    Упражнение кошелек

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-const user2 = {
-  name: 'Вася',
-  surname: 'Пупкин',
-  age: 24,
-  getFullName: function () {
-    return this.name + ' ' + this.surname;
+const wallet = {
+  balance: 0,
+  operations: [],
+  addBalance: function (value) {
+    this.balance += value;
+    this.operations.push({
+      reason: 'Пополнение',
+      sum: value,
+    });
+    console.log('Успешно пополнено')
+    return true;
+  },
+  minusBalance: function (value) {
+    if (this.balance >= value) {
+      this.balance -= value;
+      this.operations.push({
+        reason: 'Оплата налогов',
+        sum: -value,
+      });
+      console.log('Успешно оплачено');
+      return true
+    }
+    console.log('Недостаточно средств, ваш баланс: ' + this.balance);
+    return false
+  },
+  watchOperationsLength: function () {
+    console.log(this.operations.length);
+    return this.operations.length;
   }
 };
+wallet.addBalance(100);
+console.log(wallet);
+wallet.addBalance(200);
+wallet.addBalance(500);
+wallet.watchOperationsLength();
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.minusBalance(100);
+wallet.watchOperationsLength();
+console.log(wallet);
 
-console.log(user2.getFullName())
 
 
-const arr = [2, 1];
-arr.sort();
-console.log(arr);
-console.log('232323'.toLowerCase())
