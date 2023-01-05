58a1e56aad4370128bb9f005cf25e138c7862638
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 12:26:22 +0300

    Rest оператор

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const userData = ['Антон', 18, 'Москва'];
-
-function getData() {
-  return ['Антон', 18, 'Москва']
-}
-
-const userame = getData()[0];
-const userAge = getData()[1];
-const userCity = getData()[2];
-
-const [userName, _, city] = userData;
-
-console.log(userName, city);
+const data = [1, 2, 3, 4, 5, 6];
+const [one, two, ...others] = data;
+console.log(one, two, others);
