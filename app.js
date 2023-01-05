8d374392743a208e7ef294482123a410b8bde467
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 09:04:31 +0300

    Упражнения с фуормулой сложных процентов

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// const hoursPerDay = 5;
-// const daysPerWeek = 5;
-// const daysForWork = 11 - 7 + 5;
-// console.log(daysForWork * 5 >= 40);
-// const price = daysForWork  * 80;
-// console.log(price + '$');
-
-//Данные
-const payRateUSD = 80;
-const projectHours = 40;
-const availableHours = (11 - 2) * 5;
-
-//Результат
-console.log('Смогу ли я работать? ' + (availableHours > projectHours));
-console.log('Стоимость работ? ' + (projectHours * payRateUSD + '$'));
+const count = 12000;
+const procentOnYear = 7;
+const waitingMoney = 13500;
+const waitingTime = 2 * 12;
+const currentMoney = count * (1 + procentOnYear / 100 / 12) ** waitingTime;
+console.log(currentMoney)
+if (currentMoney > waitingMoney) {
+  console.log(`Сможет купить дом за ${waitingMoney}$, остаток ${currentMoney - waitingMoney}. Мы накопили ${currentMoney}`)
+} else {
+  console.log(`Не сможет купить дом за ${waitingMoney}`)
+}
