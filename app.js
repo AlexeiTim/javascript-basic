205494fb05084e8e7f7c7b63502866e28f0add88
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 13:53:26 +0300

    Возврат функции замыкание

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-function add(a, b) {
-  return a + b;
-};
-
-function subtract(a, b) {
-  return a - b;
-};
-
-function power(a, b) {
-  return a ** b;
+function power(pow) {
+  return function (num) {
+    return num ** pow;
+  }
 }
-// Функция высшего порядка
 
-function calculate(a, b, fn) {
-  console.log(fn.name);
-  const result = fn(a, b);
-  return result;
-};
 
-const res1 = calculate(3, 5, add);
-console.log(res1);
-const res = calculate(3, 5, subtract);
-console.log(res);
-const res3 = calculate(3, 5, power);
-console.log(res3);
+const powerOfTwo = power(2);
+console.log(powerOfTwo(5));
+console.log(powerOfTwo(10));
+
+const powerOfThree = power(3);
+console.log(powerOfThree(5));
+console.log(powerOfThree(10));
+
+console.log(power(5)(4))
+
