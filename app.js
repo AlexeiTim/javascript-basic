d68b63a498f30dc057716b7f1c9d614e6966e5a4
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 15:09:34 +0300

    Метод some, реализация на find

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-const arr = [2, 4, 4, 10];
+const arr = [2, 4, 4, 10, 20];
 
-let elGT5;
-for (const el of arr) {
-  if (el > 5) {
-    elGT5 = el
-    break
-  }
+const checkElement = (value, arr) => {
+  let result = arr.find(el => el == value);
+  return result == undefined ? false : true;
 }
 
-// console.log(elGT5)
-
-elGT5 = arr.find(el => el > 5);
-elGT5Index = arr.findIndex(el => el < 0);
-
-console.log(elGT5)
-console.log(elGT5Index)
+console.log(arr.some(el => el === 2))
