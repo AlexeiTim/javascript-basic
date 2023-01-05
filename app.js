bed065b723ad11770afba660904423cfa22ee7a5
AlexeiTim <timashkov060896@gmail.com>
2023-01-06 01:08:12 +0300

    Работа с DOM

--- app.js
+++ app.js
@@ -1,36 +1,5 @@
-const userInfo = {
-  balance: 0,
-  operations: 0,
-  increase(sum) {
-    this.balance += sum;
-    this.operations++;
-    console.log(this.balance, this.operations);
-  }
-}
+console.log(document);
 
-function user() {
-  const userObj = {
-    balance: 0,
-    operations: 0,
-    increase(sum) {
-      this.balance += sum;
-      this.operations++;
-      console.log(this.balance, this.operations);
-    }
-  }
-  return function () {
-    return userObj;
-  }
-}
-
-const user1 = user(userInfo);
-user1().increase(100);
-user1().increase(100);
-
-const user2 = user(userInfo);
-user2().increase(200);
-user2().increase(200);
-user2().increase(200);
-
-console.log(user1());
-console.log(user2());
+let panelText = document.querySelector('.panel').innerHTML;
+document.querySelector('.panel').innerHTML = 'New Text';
+document.querySelector('.input').value = 'Text'
