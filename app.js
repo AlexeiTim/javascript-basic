8ae7bbd655e8f3c8f1c018c4625c4756e34f3674
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 20:49:15 +0300

    Упражнение - склад

--- app.js
+++ app.js
@@ -1,16 +1,71 @@
-const cities = {
-  mask: {
-    temp: {
-      celcius: 25
+const warehouse = {
+  goods: [],
+  findGoodById: function (id) {
+    let res = this.goods.find(el => el.id === id);
+    if (!res) {
+      console.log('Такого товара нет')
+      return
+    }
+    console.log(res);
+  },
+  addGood: function (el) {
+    let id = el.id;
+    if (this.goods.some(el => el.id === id)) {
+      console.log('Такой товар уже есть');
+      return
+    } else {
+      console.log(`Товар добавлен в хранилище с id ${el.id}`);
+      this.goods.push(el);
     }
   },
-  spb: {
+  getWeightKg: function () {
+    let res = this.goods.reduce((acc, item) => {
+      let weight = item?.weight?.kg;
 
+      if (!weight) {
+        return acc
+      } else {
+        return acc += weight
+      }
+    }, 0)
+    console.log(`weight ${res}`);
+    return res
   }
 }
-const city = 'spb';
-if (cities[city] != undefined && cities[city].temp != undefined) {
-  console.log(cities[city].temp.celcius);
+
+const car = {
+  id: 1,
+  weight: {
+    kg: 1000
+  },
+  brand: 'Ford'
 }
 
-console.log(cities[city]?.temp?.celcius);
+const chair = {
+  id: 2,
+  weight: {
+    kg: 2
+  }
+}
+
+const paper = {
+  id: 3,
+  color: 'red'
+}
+
+
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+warehouse.getWeightKg();
+warehouse.findGoodById(2);
+warehouse.findGoodById(1);
+warehouse.findGoodById(3);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+warehouse.findGoodById(2);
+warehouse.findGoodById(1);
+warehouse.findGoodById(3);
+warehouse.getWeightKg();
+
+
