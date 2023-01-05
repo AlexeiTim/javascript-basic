c3b5eeb0d0cce43eb92cf02076345d5e228ad013
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 16:47:59 +0300

    Упражнение кодировка карты

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-const film = ' Звёздные Воины';
-console.log(film.padStart(50, '*'));
-console.log(film.padEnd(50, '*'));
-console.log(film.repeat(10));
-
-
+const card = '2342834503458353';
+console.log(card.slice(-4).padStart(16, '*'))
