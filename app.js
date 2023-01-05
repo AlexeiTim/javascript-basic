b7af2783b12990268cd0fe6af763de276b98e4ee
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 12:01:23 +0300

    Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+const tasks = ['Задача 1'];
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
+function addTask(task) {
+  tasks.push(task);
+  console.log(tasks);
+};
 
-console.log(roles.join('-'))
+function deleteTaskByName(name) {
+  if (tasks.includes(name)) {
+    const index = tasks.indexOf(name);
+    tasks.splice(index, 1);
+    console.log(tasks);
+  } else {
+    console.log('Нету такого элемента в массиве');
+  }
+
+}
+
+function passTaskToStartByName(name) {
+  if (tasks.includes(name)) {
+    const index = tasks.indexOf(name);
+    const el = tasks.splice(index, 1)[0];
+    tasks.unshift(el);
+    console.log(tasks);
+  } else {
+    console.log('Нету такого элемента в массиве');
+  }
+}
+
+addTask('Задача 2');
+addTask('Задача 3');
+addTask('Задача 4');
+deleteTaskByName('Задача 3');
+passTaskToStartByName('Задача 2');
+
+
