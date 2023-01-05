a2f2a8380de2b2b81b7648624a6e07c14823ce07
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 10:43:55 +0300

    Параметры по умолчанию в функциях

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-function toPower(num, power) {
+function toPower(num = 2, power = 2) {
+  // power = power || 2;
   const res = num ** power;
   return res;
 }
 
 console.log(toPower(2, 3));
-
-const toPowerArrow = (num, power) => num ** power;
-
-console.log(toPowerArrow(2, 3))
+console.log(toPower(2))
