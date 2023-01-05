a2eb5cd7dfa1e68edb4ceeccdc0083e1490f779f
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 23:38:04 +0300

    Обьявление метода объекта EOI

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-const company = {
-  name: 'ООО Агро',
-  employees: [
-    {
-      name: 'Света',
-      getEmployeesName: function () {
-        console.log(this.name);
-        return this.name
-      },
-    },
-    {
-      name: 'Катя',
-      getEmployeesName: function () {
-        console.log(this.name);
-        return this.name
-      },
-    },
-  ],
-  ceo: {
-    name: 'Вася',
-    getCeoName: function () {
-      let name = this.name;
-      console.log(name);
-    }
+'use strict';
+
+const b = 1;
+
+const a = {
+  b,
+  getB: function () {
+    console.log(this.b);
   },
-  getCompany: function () {
-    console.log(`Название компании: ${this.name}`)
-    return this.name
-  },
-
-
-}
-
-company.ceo.getCeoName();
-console.log(company.employees.map(employee => employee.getEmployeesName()))
-company.getCompany();
+  getBAlt() {
+    console.log(this.b);
+  }
+};
+a.getB();
+a.getBAlt();
+console.log(a);
