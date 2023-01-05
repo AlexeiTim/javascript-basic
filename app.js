802a6877ec5004011524cecd3470888cd118fa7f
AlexeiTim <timashkov060896@gmail.com>
2023-01-05 10:01:51 +0300

    Оператор нулевого слияния

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-console.log('Вася' || 'Олег');
-console.log(false || 'Олег');
-console.log('Вася' || false);
-console.log(false || false);
-
-console.log('Вася' && 'Олег');
-console.log(false && 'Олег');
-console.log('Вася' && false);
-console.log(false && false);
-
-
-let a;
-const userName = a || 'Петя';
-console.log(userName);
-
-const isAdmin = true;
-const filename = isAdmin && 'file.mp4';
-console.log(filename);
-
+let username = '';
+console.log(username || 'Default Username');
+console.log(username ?? 'Default Username');
 
 
