c6f790b6cdf0314d1628eb6ff7474b809e597ee6
AlexeiTim <timashkov060896@gmail.com>
2023-01-06 09:36:52 +0300

    Упражнение - получение DOM элементов

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ function submitForm() {
   document.querySelector('.panel').innerText = input;
   document.querySelector('.input').value = '';
   document.querySelector('.notification').classList.add('notification_active')
+  console.log(document.querySelector('.notification').getAttribute('class'))
+  document.querySelector('.notification').setAttribute('class', 'notification');
+  document.querySelector('.notification').setAttribute('key', 1);
+  document.querySelector('.notification').setAttribute('user-id', 1);
+  console.log(Number(document.querySelector('.notification').getAttribute('user-id')))
+
+
+
 }
 
 
@@ -22,3 +30,31 @@ function inputChanged(e) {
 //     submitForm()
 //   }
 // })
+
+
+// ; (function () {
+//   const firstAndSecondElement = document.querySelectorAll('.one');
+//   firstAndSecondElement.forEach(item => {
+//     console.log(item.firstChild.nextSibling.textContent);
+//   })
+//   const threeElement = document.querySelector('#two');
+//   console.log(threeElement.textContent.trim())
+//   const spanElements = document.querySelectorAll('span');
+//   spanElements.forEach(el => {
+//     if (el.getAttribute('user-id')) {
+//       console.log(el.textContent);
+//     }
+//   })
+// })()
+
+// console.log(document.querySelector('.one').innerText)
+// console.log(document.querySelector('.one ~ div').innerText);
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+
+console.log(document.querySelector('#two').innerText);
+
+console.log(document.getElementById('two').innerText);
+console.log(document.querySelector('[user-id="4"]').innerText);
+
+
