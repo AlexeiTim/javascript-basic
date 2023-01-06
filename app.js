2f83369c4030fb00408a19a8123adcc91af7a158
AlexeiTim <timashkov060896@gmail.com>
2023-01-06 09:43:43 +0300

    Добавление html на лету

--- app.js
+++ app.js
@@ -49,12 +49,19 @@ function inputChanged(e) {
 
 // console.log(document.querySelector('.one').innerText)
 // console.log(document.querySelector('.one ~ div').innerText);
-console.log(document.querySelectorAll('.one')[0].innerText);
-console.log(document.querySelectorAll('.one')[1].innerText);
+// console.log(document.querySelectorAll('.one')[0].innerText);
+// console.log(document.querySelectorAll('.one')[1].innerText);
 
-console.log(document.querySelector('#two').innerText);
+// console.log(document.querySelector('#two').innerText);
 
-console.log(document.getElementById('two').innerText);
-console.log(document.querySelector('[user-id="4"]').innerText);
+// console.log(document.getElementById('two').innerText);
+// console.log(document.querySelector('[user-id="4"]').innerText);
 
-
+const panelText = 'Панель';
+const panelClass = 'button';
+const newElement = document.createElement('div');
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+// newElement.innerText = 'Кнопка';
+newElement.innerHTML = `<button class='${panelClass}'>${panelText}</button>`
+document.querySelector('.test').append(newElement);
