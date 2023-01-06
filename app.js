720fc7e66b872bc2ec2ee7cdf0c6c55e86278518
AlexeiTim <timashkov060896@gmail.com>
2023-01-06 08:48:27 +0300

    Выбор и манипуляции с элементами

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log(document);
 
 let panelText = document.querySelector('.panel').innerHTML;
 document.querySelector('.panel').innerHTML = 'New Text';
-document.querySelector('.input').value = 'Text'
+document.querySelector('.input').value = 'Text';
