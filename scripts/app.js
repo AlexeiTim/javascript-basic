2810b839d5066f4b0c945d977a8eade9d32bc39a
AlexeiTim <timashkov060896@gmail.com>
2023-01-06 12:44:05 +0300

    Загрузка и сохранение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+'use strict'
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY'
+
+/* utils */
+
+function loadData() {
+  const habbitsString = localStorage.getItem(HABBIT_KEY);
+  const habbitsArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitsArray)) {
+    habbits = habbitsArray;
+  }
+
+}
+
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+;(() => {
+  loadData();
+})()
